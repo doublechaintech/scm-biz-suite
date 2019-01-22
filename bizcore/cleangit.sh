858922dcfce0ca5ab9079490c1eacff4465690ba
philipgreat <philip_chang@163.com>
2019-01-22 17:01:50 +0800

    commit for removing extra files

--- bizcore/cleangit.sh
+++ bizcore/cleangit.sh
@@ -1,21 +1,17 @@
 # !/bin/bash
 
 git add .
-git commit -m"commit for remove extra files"
+git commit -m"commit for removing extra files"
 git pull
 git push
 git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore/WEB-INF/classes' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore/WEB-INF/caf_custom_src/META-INF/infra.properties' --prune-empty --tag-name-filter cat -- --all
 git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore/WEB-INF/lib' --prune-empty --tag-name-filter cat -- --all
 git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore/README.md' --prune-empty --tag-name-filter cat -- --all
 git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore/build' --prune-empty --tag-name-filter cat -- --all
 git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizui/dist' --prune-empty --tag-name-filter cat -- --all
-
 git commit -m"remove  extra class"
 git push origin --force --all
-
-
-
-
 
 
 
