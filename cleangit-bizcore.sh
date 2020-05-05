a198608269f281e740e9643fec02d85d2ac4a58c
Philip Zhang <zhangxilai@doublechaintech.com>
2020-05-05 14:03:37 +0800

    commit for removing extra files

--- cleangit-bizcore.sh
+++ cleangit-bizcore.sh
@@ -4,7 +4,7 @@ git add .
 git commit -m"commit for removing extra files"
 git pull
 git push
-git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch bizcore2' --prune-empty --tag-name-filter cat -- --all
 git commit -m"remove  extra class"
 git push origin --force --all
 
