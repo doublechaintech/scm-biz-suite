786dccc38e79a6e814793648a5f2f5c566ff3393
Philip Z <philip_chang@163.com>
2021-09-06 16:42:46 +0800

    Do not need it anymore

--- gitinit.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-projectname="retailscm-biz-suite"
-echo "# $projectname " >> README.md
-git init
-git add -A .
-git commit -m "first commit"
-git remote add origin https://github.com/doublechaintech/$projectname.git
-git push -u origin master
